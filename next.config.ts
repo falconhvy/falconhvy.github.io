1b66fd958c3db1961354011c1251e65ee01bd13e
wdvsh <wdv.shin@gmail.com>
2025-01-25 14:10:40 +0900

    Revert "[#3] copy-webpack-plugin 설정"
    This reverts commit 0bd8245f19c4bf43de7f74ecd2f40feb00c6d7ea.

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,8 @@
 import type { NextConfig } from "next";
-import CopyPlugin from "copy-webpack-plugin";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
   // Enable static export
   output: "export",
-
-  // Custom webpack config
-  webpack: config => {
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, "contents"),
-            to: path.resolve(__dirname, "public/posts"),
-          },
-        ],
-      }),
-    );
-
-    return config;
-  },
 };
 
 export default nextConfig;
