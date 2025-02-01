b5bb0fed216d80c6f09b495f15c50891cfd82b76
wdvsh <wdv.shin@gmail.com>
2025-02-01 19:09:49 +0900

    images.unoptimized 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   // Enable static export
   output: "export",
 
+  // https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+  "images.unoptimized": true,
+
   // Custom webpack config
   webpack: config => {
     config.plugins.push(
