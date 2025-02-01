ac83ac3c96b277d3c3617433286790497bc9125b
wdvsh <wdv.shin@gmail.com>
2025-02-01 18:47:13 +0900

    프로필 문구 수정

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,7 +1,7 @@
 export default function Profile() {
   const description = `
     부담 없는 기술 공유 공간을 만들고 싶어 개인 블로그를 시작했습니다.
-    쇼핑 서비스를 개발하며 JVM, Spring 프레임워크, Hadoop 생태계에 대한 경험을 쌓아가고 있는 주니어 개발자입니다.
+    Java, Spring 프레임워크, Hadoop 생태계에 대한 경험을 쌓아가고 있는 주니어 개발자입니다.
   `.trim();
 
   return (
